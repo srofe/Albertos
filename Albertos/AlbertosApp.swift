ac475fdb589eda9d4cabe4183a9a8076919028c6
Simon Rofe <simon.rofe@ieee.org>
2021-09-22 19:32:39 +1000

    Initial Commit

---
+++ Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by Simon Rofe on 22/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
