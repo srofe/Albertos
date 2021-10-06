7e6b1955c5ba17f4335a45136894813fd6730c9e
Simon Rofe <simon.rofe@ieee.org>
2021-10-06 17:21:04 +1000

    Add requirements text to tests

--- AlbertosTests/MenuRowViewModelTests.swift
+++ AlbertosTests/MenuRowViewModelTests.swift
@@ -13,12 +13,12 @@ class MenuRowViewModelTests: XCTestCase {
     func test_WhenItmeIsNotSpicy_TextIsItemNameOnly() {
         let item: MenuItem = .fixture(name: "name", spicy: false)
         let viewModel = MenuRow.ViewModel(item: item)
-        XCTAssertEqual(viewModel.text, "name")
+        XCTAssertEqual(viewModel.text, "name", "A menu item which is not spicy shall display its name only.")
     }
 
     func test_WhenItemIsSpicy_TextIsItemWithChilliEmoji() {
         let item: MenuItem = .fixture(name: "name", spicy: true)
         let viewModel = MenuRow.ViewModel(item: item)
-        XCTAssertEqual(viewModel.text, "name 🌶")
+        XCTAssertEqual(viewModel.text, "name 🌶", "A menu item which is spicy shall display its name with a chilli emoji appended.")
     }
 }
