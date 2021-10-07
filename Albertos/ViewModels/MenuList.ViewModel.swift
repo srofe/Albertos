d3c4385ebdfca6d433d98ff5f41f8be4a176f9e1
Simon Rofe <simon.rofe@ieee.org>
2021-10-07 14:41:20 +1000

    Reformat to make easier to read

--- Albertos/ViewModels/MenuList.ViewModel.swift
+++ Albertos/ViewModels/MenuList.ViewModel.swift
@@ -19,13 +19,14 @@ extension MenuList {
         ) {
             menuFetching
                 .fetchMenu()
-                .sink(receiveCompletion: { [weak self] completion in
-                    guard case .failure(let error) = completion else { return }
-                    self?.sections = .failure(error)
-                },
-                      receiveValue: { [weak self] value in
-                    self?.sections = .success(menuGrouping(value))
-                })
+                .sink(
+                    receiveCompletion: { [weak self] completion in
+                        guard case .failure(let error) = completion else { return }
+                        self?.sections = .failure(error)
+                    },
+                    receiveValue: { [weak self] value in
+                        self?.sections = .success(menuGrouping(value))
+                    })
                 .store(in: &cancellables)
         }
     }
